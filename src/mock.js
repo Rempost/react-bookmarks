5e0a64464cab750a6817428c7d1e5da0b3b49744
Evgenii Riasov <evgenii_riasov@epam.com>
2017-02-06 12:59:20 +0400

    added backend mock for bookmarks

---
+++ src/mock.js
@@ -0,0 +1,10 @@
+export default bookmarksMock = [
+    {
+        title: 'A Beginner’s Guide to Webpack 2 and Module Bundling',
+        url: 'https://www.sitepoint.com/beginners-guide-to-webpack-2-and-module-bundling'
+    },
+    {
+        title: 'Presentational and Container Components',
+        url: 'https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0#.3qnrztutf'
+    }
+];
